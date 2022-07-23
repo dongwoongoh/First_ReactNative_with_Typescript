a2554de92778d1599a0da30297c9b308b6fc514d
maaaaaaaaad <integral5137@gmail.com>
2022-07-24 00:28:41 +0900

    custom header on navigation

--- App.tsx
+++ App.tsx
@@ -1,8 +1,67 @@
 import * as React from 'react';
+import {Pressable, Text, View} from 'react-native';
 import {NavigationContainer} from '@react-navigation/native';
+import {
+  createNativeStackNavigator,
+  NativeStackScreenProps,
+} from '@react-navigation/native-stack';
+
+type RootStackParamList = {
+  Home: undefined;
+  Details: {
+    message: string;
+  };
+};
+
+function HomeScreen({
+  navigation,
+}: NativeStackScreenProps<RootStackParamList, 'Home'>) {
+  return (
+    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+      <Pressable
+        onPress={() =>
+          navigation.navigate('Details', {message: 'Hello World!'})
+        }>
+        <Text>Home Screen</Text>
+      </Pressable>
+    </View>
+  );
+}
+
+function DetailsScreen({
+  route,
+  navigation,
+}: NativeStackScreenProps<RootStackParamList, 'Details'>) {
+  const {message} = route.params;
+  return (
+    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+      <Pressable onPress={() => navigation.navigate('Home')}>
+        <Text>Details Screen Message: {message}</Text>
+      </Pressable>
+    </View>
+  );
+}
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <NavigationContainer>{/* Rest of your app code */}</NavigationContainer>
+    <NavigationContainer>
+      <Stack.Navigator
+        initialRouteName="Home"
+        screenOptions={{
+          headerStyle: {backgroundColor: '#ff9900'},
+          headerTitleStyle: {color: '#ffff'},
+        }}>
+        <Stack.Screen
+          name="Home"
+          component={HomeScreen}
+          options={{
+            title: 'Robby',
+          }}
+        />
+        <Stack.Screen name="Details" component={DetailsScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
