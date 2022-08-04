063898b2bb1f159024e5869b6ad6de4274d51ea0
maaaaaaaaad <integral5137@gmail.com>
2022-08-04 22:15:17 +0900

    top tab stack params

--- src/@types/index.d.ts
+++ src/@types/index.d.ts
@@ -1,3 +1,10 @@
 type RootStackParamList = {
   readonly terminal: undefined;
+  readonly topTabNavigate: keyof TopTabStackParamList;
+};
+
+type TopTabStackParamList = {
+  readonly lounge: undefined;
+  readonly details: undefined;
+  readonly my: undefined;
 };
