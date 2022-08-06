33aabcd9398dc26620fa5ebfbe19abbe6f2fbef3
maaaaaaaaad <integral5137@gmail.com>
2022-08-06 19:26:03 +0900

    add custom top tab navigate routes

--- App.tsx
+++ App.tsx
@@ -1,15 +1,10 @@
 import * as React from 'react';
 import SplashScreen from 'react-native-splash-screen';
-import {SafeAreaView} from 'react-native-safe-area-context';
 import BottomTabNavigate from './src/navigations/bottom.tab.navigate';
 
 export default function App() {
   React.useEffect(() => {
     SplashScreen.hide();
   }, []);
-  return (
-    <SafeAreaView>
-      <BottomTabNavigate />
-    </SafeAreaView>
-  );
+  return <BottomTabNavigate />;
 }
--- src/navigations/bottom.tab.navigate.tsx
+++ src/navigations/bottom.tab.navigate.tsx
@@ -3,16 +3,22 @@ import {createMaterialTopTabNavigator} from '@react-navigation/material-top-tabs
 import lounge from '../screens/lounge/lounge';
 import rooms from '../screens/rooms/rooms';
 import profile from '../screens/profile/profile';
+import {NavigationContainer} from '@react-navigation/native';
+import TopTabBar from '../components/topTabBar/top.tab.bar';
 
 const Bottom = createMaterialTopTabNavigator<RootNavigateStackParamList>();
 
 function BottomTabNavigate() {
   return (
-    <Bottom.Navigator tabBarPosition="bottom">
-      <Bottom.Screen name="lounge" component={lounge} />
-      <Bottom.Screen name="rooms" component={rooms} />
-      <Bottom.Screen name="profile" component={profile} />
-    </Bottom.Navigator>
+    <NavigationContainer>
+      <Bottom.Navigator
+        tabBar={props => <TopTabBar {...props} />}
+        tabBarPosition="top">
+        <Bottom.Screen name="lounge" component={lounge} />
+        <Bottom.Screen name="rooms" component={rooms} />
+        <Bottom.Screen name="profile" component={profile} />
+      </Bottom.Navigator>
+    </NavigationContainer>
   );
 }
 
