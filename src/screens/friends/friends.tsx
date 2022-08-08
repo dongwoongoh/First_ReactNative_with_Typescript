36891483926472e774ac3524d275c33bc258b640
maaaaaaaaad <integral5137@gmail.com>
2022-08-08 22:03:33 +0900

    set custom top bar

---
+++ src/screens/friends/friends.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Text, View} from 'react-native';
+
+function Friends() {
+  return (
+    <View>
+      <Text>Friends</Text>
+    </View>
+  );
+}
+
+export default Friends;
